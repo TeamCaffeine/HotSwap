fc83fd973a3e948817a50bc16e09758c5c649ec3
Megan Fantes <meganfantes@gmail.com>
2017-11-14 15:07:41 -0500

    added mavenCentral() repository and multiple facebook dependencies. also updated the facebook connection to our app and the facebook connection to firebase, which you can't see in the code. nothing is happening upon logging in with facebook, but I think that has to do with intents. also, the user still isn't being added to the authenticated users on Firebase, and I don't know why.

--- app/src/main/java/com/teamcaffeine/hotswap/FacebookLoginActivity.java
+++ app/src/main/java/com/teamcaffeine/hotswap/FacebookLoginActivity.java
@@ -14,6 +14,7 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
@@ -46,6 +47,7 @@ public class FacebookLoginActivity extends BaseActivity implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_facebook);
 
         // Views
