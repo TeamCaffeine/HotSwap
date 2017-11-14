6151f2d01c1a9dfc03b13cbf22dfd6faa2607a4e
Megan Fantes <meganfantes@gmail.com>
2017-11-14 12:05:40 -0500

    Facebook Login having the same problem as google sign in: brings user to sign in page, but won't complete the action of login on. The sign in button shows up, but clicking it doesn't acutally login the user. Also, added a facebook logout button to the activity_home layout, thinking maybe the FacebookLloginActivity spawned an intent that I would point to the HomeActivity, but right now it does not do that.

--- app/src/main/java/com/teamcaffeine/hotswap/HomeActivity.java
+++ app/src/main/java/com/teamcaffeine/hotswap/HomeActivity.java
@@ -1,7 +1,12 @@
 package com.teamcaffeine.hotswap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.login.LoginManager;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button logout = (Button) findViewById(R.id.btnFacebookLogout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logOut();
+                Intent logout = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(logout);
+                finish();
+            }
+        });
     }
 }
