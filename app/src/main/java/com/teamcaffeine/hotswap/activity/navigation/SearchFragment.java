6b43148f64ce5b1da2a989f514dd15afe77b02ac
Tkixi <tkixi@users.noreply.github.com>
2017-11-27 22:08:00 -0500

    Get Maps Fragment to appear within the Search fragment

--- app/src/main/java/com/teamcaffeine/hotswap/activity/navigation/SearchFragment.java
+++ app/src/main/java/com/teamcaffeine/hotswap/activity/navigation/SearchFragment.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.teamcaffeine.hotswap.R;
-import com.teamcaffeine.hotswap.activity.messaging.InboxFragment;
 
 /**
  * A simple {@link Fragment} subclass.
