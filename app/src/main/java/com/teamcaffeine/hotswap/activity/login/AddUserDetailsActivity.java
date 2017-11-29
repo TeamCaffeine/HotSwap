b8f3255707853157d519c2955bb6047a0cc68ba4
Megan Fantes <meganfantes@gmail.com>
2017-11-29 18:13:41 -0500

    updated Login activity, Add User Details Activity, and layout of Add User Details activity

--- app/src/main/java/com/teamcaffeine/hotswap/activity/login/AddUserDetailsActivity.java
+++ app/src/main/java/com/teamcaffeine/hotswap/activity/login/AddUserDetailsActivity.java
@@ -2,9 +2,11 @@ package com.teamcaffeine.hotswap.activity.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,11 +20,15 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AddUserDetailsActivity extends BaseLoginActivity {
+public class AddUserDetailsActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     private FirebaseUser user;
     private EditText firstName;
     private EditText lastName;
+    private EditText phoneNumber;
+    private Button addAddress;
+    private Button addPayment;
     private Button submit;
 
     @Override
@@ -30,37 +36,36 @@ public class AddUserDetailsActivity extends BaseLoginActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user_details);
 
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
         firstName = findViewById(R.id.edtFirstName);
         lastName = findViewById(R.id.edtLastName);
+        phoneNumber = findViewById(R.id.edtPhoneNumber);
+        addAddress = findViewById(R.id.btnAddress);
+        addPayment = findViewById(R.id.btnPayment);
         submit = findViewById(R.id.btnSubmit);
-
-        user = getCurrentUser();
 
         String firstName_string = firstName.getText().toString();
         String lastName_string = lastName.getText().toString();
-
-        Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+        String phoneNumber_string = phoneNumber.getText().toString();
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference users = database.getReference().child("Users");
 
+        boolean addedDetails = true;
         String uid = user.getUid();
         String email = user.getEmail();
         DateFormat dateFormat = new SimpleDateFormat("MMMM d, yyyy");
-        Date dateCreated = new Date();
-        User userData = new User(uid, email, dateFormat.format(dateCreated), firstName_string, lastName_string,
-                new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>(),
-                new ArrayList<String>(), new ArrayList<String>());
+        Date memberSince = new Date();
+        User userData = new User(addedDetails,uid,firstName_string, lastName_string,email,dateFormat.format(memberSince),phoneNumber_string);
 
         Map<String, Object> userUpdate = new HashMap<>();
         userUpdate.put(uid, userData.toMap());
 
         users.updateChildren(userUpdate);
 
-        i.putExtra("email", user.getEmail());
-        i.putExtra("Uid", uid);
-        i.putExtra("fullName", firstName_string + " "  + lastName_string);
-        i.putExtra("dateCreated", dateFormat.format(dateCreated));
+        Intent i = new Intent(getApplicationContext(), HomeActivity.class);
         startActivity(i);
     }
 }
--- app/src/main/java/com/teamcaffeine/hotswap/activity/login/LoginActivity.java
+++ app/src/main/java/com/teamcaffeine/hotswap/activity/login/LoginActivity.java
@@ -2,31 +2,57 @@ package com.teamcaffeine.hotswap.activity.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.teamcaffeine.hotswap.R;
 import com.teamcaffeine.hotswap.activity.HomeActivity;
 
-public class LoginActivity extends BaseLoginActivity {
+public class LoginActivity extends AppCompatActivity {
 
     SignInButton btnGoogleSignIn;
     LoginButton btnFacebookLogin;
     Button btnEmailLogin;
 
+    private FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+    FirebaseDatabase database;
+    DatabaseReference users;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        FirebaseUser currentUser = getCurrentUser();
-        if (currentUser != null) {
-            Intent i = new Intent(this, HomeActivity.class);
-            startActivity(i);
-        }
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // get Firebase authorization instance
+        mAuth = FirebaseAuth.getInstance();
+
+        // get reference to user
+        currentUser = mAuth.getCurrentUser();
+
+        // get instance of the database
+        database = FirebaseDatabase.getInstance();
+        // get reference to the users table
+        users = database.getReference("Users");
+
+
+
+
+
+
+
 
         btnGoogleSignIn = (SignInButton) findViewById(R.id.google_sign_in_button);
         btnFacebookLogin = (LoginButton) findViewById(R.id.facebook_login_button);
@@ -55,5 +81,14 @@ public class LoginActivity extends BaseLoginActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void onStart() {
+        super.onStart();
+        if (currentUser != null) {
+            final Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(i);
+        }
+
     }
 }
