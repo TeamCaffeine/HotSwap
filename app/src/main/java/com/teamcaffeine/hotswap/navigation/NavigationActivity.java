e6838ec44aeee89cb4f2c08647b3a973eeb67ff3
williamieong <ieong24861@yahoo.com>
2017-12-02 12:32:18 -0500

    Removed log statements and testing code

--- app/src/main/java/com/teamcaffeine/hotswap/navigation/NavigationActivity.java
+++ app/src/main/java/com/teamcaffeine/hotswap/navigation/NavigationActivity.java
@@ -12,20 +12,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 
-import com.firebase.client.Firebase;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 import com.teamcaffeine.hotswap.R;
 import com.teamcaffeine.hotswap.utility.SessionHandler;
 
 import java.lang.reflect.Field;
-import com.teamcaffeine.hotswap.navigation.ListItemFragment;
-import com.teamcaffeine.hotswap.navigation.ProfileFragment;
-import com.teamcaffeine.hotswap.navigation.InboxFragment;
 import com.teamcaffeine.hotswap.messaging.ChatFragment;
 
 public class NavigationActivity extends AppCompatActivity implements
@@ -89,38 +79,6 @@ public class NavigationActivity extends AppCompatActivity implements
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
-
-        Log.e("***", "BEFORE DATABASE");
-//        FirebaseDatabase kappa = FirebaseDatabase.getInstance();
-//        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-//            Log.e("***", "ITS NOT NULL MY BOI");
-//        } else {
-//            Log.e("***", "ITS NULL MY BOI");
-//        }
-//        Log.e("***", String.valueOf(kappa.getReference()));
-
-        FirebaseAuth.getInstance().signInWithEmailAndPassword("william@william.com","william").addOnFailureListener(
-                this, new OnFailureListener() {
-
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e("***", "DID NOT WORK, FAILURE LISTENER");
-                        Log.e("***", e.toString());
-                    }
-                }
-        ).addOnCompleteListener(this,
-                new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()) {
-                            Log.e("***", FirebaseAuth.getInstance().getCurrentUser().getEmail());
-                        } else {
-                            Log.e("***", "DID NOT WORK");
-                        }
-                    }
-                });
-
-        Log.e("***", "AFTER DATABASE");
 
         navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
