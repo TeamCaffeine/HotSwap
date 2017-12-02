c651177267c2ef76f6554521d4a1bea57d0e959a
Megan Fantes <meganfantes@gmail.com>
2017-12-02 13:52:54 -0500

    begin adding ListView adapter to profile fragment to display addresses and payment options. added layout file for listview row

--- app/src/main/java/com/teamcaffeine/hotswap/navigation/ProfileFragment.java
+++ app/src/main/java/com/teamcaffeine/hotswap/navigation/ProfileFragment.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.PopupWindow;
 import android.widget.TextView;
@@ -247,4 +248,27 @@ public class ProfileFragment extends Fragment {
         });
     }
 
+}
+
+class ListAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
 }
