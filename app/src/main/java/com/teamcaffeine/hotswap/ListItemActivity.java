17d944ba3a9b9bc6d2b5af0519576fae08351e9f
Megan Fantes <meganfantes@gmail.com>
2017-12-03 19:44:22 -0500

    added List Item Activity

---
+++ app/src/main/java/com/teamcaffeine/hotswap/ListItemActivity.java
@@ -0,0 +1,58 @@
+package com.teamcaffeine.hotswap;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.squareup.timessquare.CalendarPickerView;
+import com.teamcaffeine.hotswap.R;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class ListItemActivity extends AppCompatActivity {
+
+    private CalendarPickerView calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_list_item);
+
+        Calendar nextYear = Calendar.getInstance();
+        nextYear.add(Calendar.YEAR, 1);
+
+        calendar = (CalendarPickerView) findViewById(R.id.calendar_view);
+        Date today = new Date();
+        calendar.init(today, nextYear.getTime())
+                .withSelectedDate(today)
+                .inMode(CalendarPickerView.SelectionMode.MULTIPLE);
+
+        calendar.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Disallow ScrollView to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(true);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        // Allow ScrollView to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+
+                v.onTouchEvent(event);
+                return true;
+            }
+        });
+    }
+}
