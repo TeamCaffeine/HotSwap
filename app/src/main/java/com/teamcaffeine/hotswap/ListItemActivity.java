db0e15e973072687d8ed909982b95fa886da15e8
Megan Fantes <meganfantes@gmail.com>
2017-12-05 14:34:20 -0500

    added data validation to the List Item activity and commented the code in the Home Fragment

--- app/src/main/java/com/teamcaffeine/hotswap/ListItemActivity.java
+++ app/src/main/java/com/teamcaffeine/hotswap/ListItemActivity.java
@@ -3,12 +3,14 @@ package com.teamcaffeine.hotswap;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -20,9 +22,12 @@ import com.google.firebase.database.ValueEventListener;
 import com.squareup.timessquare.CalendarPickerView;
 import com.teamcaffeine.hotswap.swap.Item;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ListItemActivity extends AppCompatActivity {
@@ -39,6 +44,7 @@ public class ListItemActivity extends AppCompatActivity {
     private EditText editAddress;
     private Button listItemButton;
     private CalendarPickerView calendar;
+    private List<String> itemList = new ArrayList<String>();
 
     private int RESULT_ERROR = 88;
 
@@ -59,6 +65,14 @@ public class ListItemActivity extends AppCompatActivity {
         editAddress = (EditText) findViewById(R.id.editAddress);
         calendar = (CalendarPickerView) findViewById(R.id.calendarView);
         listItemButton = (Button) findViewById(R.id.listItemButton);
+
+        // get the  current list of the user's items from the intent
+        Bundle extras = getIntent().getExtras();
+        itemList = extras.getStringArrayList("itemList");
+
+        Intent intent = getIntent();
+        Bundle args = intent.getBundleExtra("BUNDLE");
+        itemList = (ArrayList<String>) args.getSerializable("itemList");
 
         Calendar nextYear = Calendar.getInstance();
         nextYear.add(Calendar.YEAR, 1);
@@ -92,41 +106,52 @@ public class ListItemActivity extends AppCompatActivity {
         listItemButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int[] submitStatus = submit();
-
-                Intent i = new Intent();
-
-
-                // get item name to send back to Home Fragment
-                String itemName = editItemName.getText().toString();
-                i.putExtra("itemName", itemName);
-
-                // Set the result with this data, and finish the activity
-                setResult(submitStatus[0], i);
-                finish();
-            }
-        });
-    }
-
-    private int[] submit() {
-        final int[] resultCode = new int[1];
-        items.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
                 String itemID = items.push().getKey();
                 String itemName = editItemName.getText().toString();
                 String itemAddress = editAddress.getText().toString();
                 String itemPrice = editPrice.getText().toString();
                 String itemDescription = editDescription.getText().toString();
 
-                Item item = new Item(itemID, itemName, firebaseUser.getUid(), itemDescription, itemPrice, itemAddress);
+                Item newItem = new Item(itemID, itemName, firebaseUser.getUid(), itemDescription, itemPrice, itemAddress);
 
+                // DATA VALIDATION
+                // a user cannot list 2 items with the same name
+                // if they enter a new item with the same name as an existing item, an Toast
+                // will show with an error message
+                if (itemList.contains(itemName)) {
+                    Toast.makeText(getBaseContext(), R.string.duplicate_item, Toast.LENGTH_LONG).show();
+                } else {
+                    // if the user is adding a new item with a new name, submit it to the database
+                    int[] submitStatus = submit(newItem);
+
+                    // create an intent to send back to the HomeActivity
+                    Intent i = new Intent();
+
+                    // send the updated itemList back to the Home Fragment
+                    i.putExtra("newItem", itemName);
+
+                    // Set the result with this data, and finish the activity
+                    setResult(submitStatus[0], i);
+                    finish();
+                }
+            }
+        });
+    }
+
+    private int[] submit(Item item) {
+        final int[] resultCode = new int[1];
+        final Item newItem = item;
+        final String itemID = newItem.getItemID();
+        final String itemName = newItem.getName();
+
+        items.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
                 Map<String, Object> itemUpdate = new HashMap<>();
-                itemUpdate.put(itemID, item.toMap());
-
-                //TODO: Add data validation so a user cannot enter an item with the same name as another item they have entered
+                itemUpdate.put(itemID, newItem.toMap());
                 items.updateChildren(itemUpdate);
 
+                // set the result code to indicate that the item was successfully added
                 resultCode[0] = RESULT_OK;
             }
 
--- app/src/main/java/com/teamcaffeine/hotswap/navigation/HomeFragment.java
+++ app/src/main/java/com/teamcaffeine/hotswap/navigation/HomeFragment.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -37,6 +38,7 @@ import com.teamcaffeine.hotswap.R;
 import com.teamcaffeine.hotswap.login.User;
 import com.teamcaffeine.hotswap.swap.Item;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -46,7 +48,11 @@ import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
 
 /**
- * A simple {@link Fragment} subclass.
+ * The Home Fragment is the landing page for the user when they first open the app.
+ * Its purpose is to display all of ht items they have listed and show what items they
+ * are currently renting and lending. This is also the page where they have the option
+ * to list new items with the "list items" button. This button takes them to the List
+ * Item Activity.
  */
 public class HomeFragment extends Fragment {
 
@@ -64,6 +70,7 @@ public class HomeFragment extends Fragment {
     private int LIST_ITEM_REQUEST_CODE = 999;
     private int RESULT_ERROR = 88;
 
+    // progress dialog to show page is loading
     public ProgressDialog mProgressDialog;
 
     // Database reference fields
@@ -73,6 +80,7 @@ public class HomeFragment extends Fragment {
     private String userTable = "users";
     private String itemTable = "items";
 
+    // progress dialog to show page is loading
     public void showProgressDialog() {
         if (mProgressDialog == null) {
             mProgressDialog = new ProgressDialog(getContext());
@@ -83,12 +91,14 @@ public class HomeFragment extends Fragment {
         mProgressDialog.show();
     }
 
+    // hide progress dialog once page loads
     public void hideProgressDialog() {
         if (mProgressDialog != null && mProgressDialog.isShowing()) {
             mProgressDialog.dismiss();
         }
     }
 
+    // fragment listener for inter-fragment communication from the Navigation Activity
     private HomeFragmentListener HFL;
 
     public HomeFragment() {
@@ -110,47 +120,72 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Log.i(TAG, "onViewCreated");
 
+        // get the view from the layout
         btnListItem = view.findViewById(R.id.btnListItem);
         listviewAllItems = view.findViewById(R.id.listviewAllItems);
         txtCurrentlyRenting = view.findViewById(R.id.txtCurrentlyRenting);
         listviewLending = view.findViewById(R.id.listviewLending);
 
-        // Get a reference to our user
+        // Get a database reference to our user
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         database = FirebaseDatabase.getInstance();
         users = database.getReference().child(userTable);
 
+        // instantiate the list that wil hold all of the user's items
         itemsElementsList = new ArrayList<String>();
 
+        // DELETE AN ITEM
+        // This onClick listener allows the user the delete an item when they click on it
+        // in the list view.
         listviewAllItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                // when the item is clicked, an alert dialog shows asking the user if they acutally
+                // want to delete the item
                 AlertDialog myQuittingDialogBox = new AlertDialog.Builder(getContext())
                         //set message, title, and icon
                         .setTitle(R.string.delete)
                         .setMessage(R.string.delete_item_question)
                         .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
-
+                            // if they click the "delete" button, delete the item
                             public void onClick(DialogInterface dialog, int whichButton) {
-                                //This call works, BUT it will delete all items with the same name and the chosen item to delete
-                                // i.e. we need data validation where a user can't enter items with same name
+                                //TODO: data validation
+                                // get the name of the item to be deleted
                                 final String itemToRemove = listviewAllItems.getItemAtPosition(position).toString();
+                                // get a reference to the items table in the database, where the item also needs to be removed
                                 DatabaseReference items = database.getReference().child(itemTable);
+                                // add a listener to the items table in the database to delete the item
                                 items.addListenerForSingleValueEvent(new ValueEventListener() {
                                     @Override
+                                    // get a snapshot of the ENTIRE items table, so the onDataChange will detect when an
+                                    // item is deleted from the items table (since items are not stored per user, every item
+                                    // is stored in the data table with a reference to the appropriate user as one of the
+                                    // attributes)
                                     public void onDataChange(DataSnapshot dataSnapshot) {
+                                        // loop through all of the items in the table from the dataSnapshot
                                         for (DataSnapshot i: dataSnapshot.getChildren()) {
+                                            // create an item object so you can read the item's contents
                                             Item item = i.getValue(Item.class);
+                                            // check if the item you are currently looping through belongs to the current user
                                             if (item.getOwnerID().equals(firebaseUser.getUid())){
+                                                // if the item does belong to the user, get its name
                                                 String iteratingItem = item.getName();
+                                                // if the item you are currenlty looping thorugh is the item the user wants to remove,
+                                                // remove it
                                                 if (iteratingItem.equalsIgnoreCase(itemToRemove)){
+                                                    // first remove it from the item list for the listView in the UI
+                                                    // the boolean didRemove will indicate when the item has been successfully
+                                                    // deleted from the list. Only 0nce it has been successfully deleted from
+                                                    // the UI, delete it from the database.
                                                     boolean didRemove = itemsElementsList.remove(listviewAllItems.getItemAtPosition(position).toString());
                                                     if (didRemove) {
+                                                        // notify the listView adapter that the data has changed so that the UI is updated
                                                         itemsAdapter.notifyDataSetChanged();
 
-                                                        // Update database
+                                                        // Remove the item and update database
                                                         i.getRef().removeValue();
                                                     } else {
+                                                        // if the item was NOT successfully removed from the item list, log the error message
                                                         Log.i(TAG, "User attempted to delete a nonexistent item");
                                                     }
                                                 }
@@ -158,104 +193,156 @@ public class HomeFragment extends Fragment {
                                         }
                                     }
 
+                                    // if the user cancels the operation, log the error message
                                     @Override
                                     public void onCancelled(DatabaseError databaseError) {
-                                        Log.e(TAG, "Payment update failed", databaseError.toException());
+                                        Log.e(TAG, "Item update failed", databaseError.toException());
                                     }
                                 });
+                                // after the item has been deleted, dismiss the alert dialog
                                 dialog.dismiss();
                             }
                         })
+                        // if the user clicks "cancel" instead of "delete," dismiss the dialog and do not
+                        // do anything to the database
                         .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 dialog.dismiss();
                             }
                         })
+                        // once all of the above functionality had been set create the alert dialog box
                         .create();
+                // once the dialog box has been created, show it
                 myQuittingDialogBox.show();
             }
         });
 
 
-
-//        // Query for all items listed by the user
-//        Query allUserItems = items.orderByChild("ownerID").equalTo(firebaseUser.getUid());
-
-        // Add listener for Firebase response on the query
-
+        // ADDING THE ITEM LIST TO THE UI
+        // get a reference to the items table in the database
         DatabaseReference items = database.getReference().child(itemTable);
+        // add a listener to the items table
         items.addListenerForSingleValueEvent( new ValueEventListener(){
             @Override
+            // get a data snapshot of the whole table
             public void onDataChange(DataSnapshot dataSnapshot) {
+                // loop through all of the items in the items table
+                // we need to loop thorugh all of the items become items do not belong to users,
+                // each items is in the table and the appropriate user is stored as an attribute.
+                // When we want to retrieve a list of a user's items, we need to loop thorugh all
+                // of them and select the user's items.
                 for(DataSnapshot i : dataSnapshot.getChildren() ){
+                    // create an item objecct to read each item's contents
                     Item item = i.getValue(Item.class);
+                    // if the item belongs to the user, add it to the list of the user's items
                     if (item.getOwnerID().equals(firebaseUser.getUid())){
                         itemsElementsList.add(item.getName());
                     }
                 }
+                // once we have a list of the user's items, create the listview adapater
                 itemsAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, itemsElementsList);
+                // set tbe adapter on the listview in the UI
                 listviewAllItems.setAdapter(itemsAdapter);
+                // notify the adapter that the dataset has changed so that it shows the new
+                // list of the user's items
                 itemsAdapter.notifyDataSetChanged();
             }
 
+            // if the read of the items table failed, log the error message
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 System.out.println("The read failed: " + databaseError.getCode());
             }
         });
 
-
+        // LIST A NEW ITEM
+        // set an onClick listener for the List Item button
         btnListItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // spawn an intent to the List Item Activity
                 Intent i = new Intent(getActivity(), ListItemActivity.class);
+                // send the current list of items with the intent
+                // so when a new item is added, it is added to the list
+                Bundle args = new Bundle();
+                args.putSerializable("itemList",(Serializable)itemsElementsList);
+                i.putExtra("BUNDLE",args);
+
+
+                // start the Activity with the appropriate request code
                 startActivityForResult(i, LIST_ITEM_REQUEST_CODE);
             }
         });
     }
 
+    // required constructer for the interface for inter-fragment communication
     public interface HomeFragmentListener {
     }
 
+    // attach the listener for inter-fragment communcation with the Navigation Activity
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         HFL = (HomeFragment.HomeFragmentListener) context;  //context is a handle to the main activity, let's bind it to our interface.
     }
 
-    //TODO: Figure out why the list of items doesn't get updated after adding a new item, but
-    // shows all items, including added ones, when the Home Fragment is built in onCreate
+    //TODO: Figure out why the list of items doesn't get updated after adding a new item, but shows all items, including added ones, when the Home Fragment is built in onCreate
+    // ADDING A NEW ITEM
+    // Once the user lists a new item in the List Item Activity, it will be added to their items list
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.i(TAG, "onActivityResult called");
         super.onActivityResult(requestCode, resultCode, data);
+        // The intent to list a new item was called with startActivityForResult, so we used a request code.
+        // here we make sure that we are getting a response only from the intent that we called with
+        // our request code.
         if (requestCode == LIST_ITEM_REQUEST_CODE) {
             Log.i(TAG, "request code = List Item Request Code");
+            // startActivityForResult also returns a result code, which checks the result of the List Item Activity.
+            // Here, RESULT_OK indicates that the item was successfully added to the database. We want to make sure
+            // that items are only added to the UI if they are added to the database, and vice versa.
             if (resultCode == RESULT_OK) {
                 Log.i(TAG, "result OK");
-                // Add listener for Firebase response on the query
-                DatabaseReference items = database.getReference().child(itemTable);
-                Log.i(TAG, "items table reference created");
-                items.addListenerForSingleValueEvent( new ValueEventListener(){
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        Log.i(TAG, "onDataChange called");
-                        for(DataSnapshot i : dataSnapshot.getChildren() ){
-                            Item item = i.getValue(Item.class);
-                            if (item.getOwnerID().equals(firebaseUser.getUid())){
-                                itemsElementsList.add(item.getName());
-                            }
-                        }
-                        itemsAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, itemsElementsList);
-                        listviewAllItems.setAdapter(itemsAdapter);
-                        itemsAdapter.notifyDataSetChanged();
-                    }
+//                // set the items list equal to the updated items list that we got in the callback
+//                // from the List Item Activity
+//                 Bundle args = data.getBundleExtra("BUNDLE");
+//                itemsElementsList = (ArrayList<String>) args.getSerializable("updatedItemList");
+                // get the new item that was added in List Item from the Intent
+                Bundle extras = data.getExtras();
+                String newItem = extras.getString("newItem");
+                // add the new item to the list
+                itemsElementsList.add(newItem);
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        System.out.println("The read failed: " + databaseError.getCode());
-                    }
-                });
+                // notify the adapter that the dataset has changed so that it updates the UI
+                itemsAdapter.notifyDataSetChanged();
+//                // get a reference to the items table in the database
+//                DatabaseReference items = database.getReference().child(itemTable);
+//                Log.i(TAG, "items table reference created");
+//                // add a listener to the items table
+//                items.addListenerForSingleValueEvent( new ValueEventListener(){
+//                    @Override
+//                    // get a dataSnapshot of all items in the table
+//                    public void onDataChange(DataSnapshot dataSnapshot) {
+//                        Log.i(TAG, "onDataChange called");
+//                        // loop through all items in the table
+//                        for(DataSnapshot i : dataSnapshot.getChildren() ){
+//                            // create an item object to read each item's contents
+//                            Item item = i.getValue(Item.class);
+//                            if (item.getOwnerID().equals(firebaseUser.getUid())){
+//                                itemsElementsList.add(item.getName());
+//                            }
+//                        }
+//                        itemsAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, itemsElementsList);
+//                        listviewAllItems.setAdapter(itemsAdapter);
+//                        itemsAdapter.notifyDataSetChanged();
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(DatabaseError databaseError) {
+//                        System.out.println("The read failed: " + databaseError.getCode());
+//                    }
+//                });
+                // show a toast to notify the user that their item was successfully added
                 Toast.makeText(getContext(), "New item added", Toast.LENGTH_LONG).show();
 
             } else if (resultCode == RESULT_ERROR) {
